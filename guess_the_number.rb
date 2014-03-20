4920680f85d2766e12f0dcea54a62f5a38df6ad2
joshuasantiago <js520702@gmail.com>
2014-03-20 17:55:46 +0000

    Fix a bug in code that gave an error

--- guess_the_number.rb
+++ guess_the_number.rb
@@ -1,19 +1,17 @@
 puts "Welcome to guess the number"
 secret_number = rand(6)
-
-puts "guess a number between 0 and 5:"
+puts "guess the number:"
 guess = gets.chomp.to_i
-
 until guess == secret_number
     puts "Wrong try again"
     guess = gets.chomp.to_i
 end
-  #3.times do 
-  #if secret_number == guess
-   # puts "Congratulations! you are correct"
-    
-  #else
-   # puts "Wrong try again"
-    #guess = gets.chomp.to_i
-  #end
-#end
+  3.times do 
+    if secret_number == guess
+    puts "Congratulations! you did it!"
+  else
+    3.times do
+      puts "Wrong #{secret_number} is right!"
+    guess = gets.chomp.to_i
+  end
+end
